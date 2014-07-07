a7da76f113acb23b780b29ca5d9dcb51a4f05493
George Villasboas <george@logics.com.br>
2014-07-07 00:13:33 -0300

    Initial Commit

---
+++ CloudKitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudKitTest
+//
+//  Created by George Henrique Villasboas on 7/7/14.
+//  Copyright (c) 2014 CocoaHeads Goiânia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
